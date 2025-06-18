b5711db85fa8901514ac9394fe49b00f7169daa1
Noah Kvirkvelia <kvirkvelianoah@gmail.com>
2025-06-18 11:09:27 +0400

    product info

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// numbers with sum and average
 let numbers = [11, 22, 33, 44, 55];
 let sum = 0;
 for (let i = 0; i < numbers.length; i++) {
@@ -10,3 +11,15 @@ document.getElementById("sum").textContent = sum;
 document.getElementById("average").textContent = average;
 console.log(sum)
 console.log(average)
+
+// Product
+let device = {
+    productName: "iPhone 15 Pro",
+    memory: ["128GB","256GB","512GB", "1TB"],
+    isAvailableAt:[
+        "21 Chavchavadze St, Batumi 6010", 
+        "22 Tbel-Abuseridze St, Batumi", 
+        "29 Sherif Khimshiashvili St, Batumi 6000"]
+};
+console.log(`${device.productName} with ${device.memory[1]} memory is available at ${device.isAvailableAt[0]}.`);
+document.getElementById("iphone").innerText = `${device.productName} with ${device.memory[1]} memory is available at ${device.isAvailableAt[0]}.`;
